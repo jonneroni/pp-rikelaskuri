6973ba529f1dd24c0b3ee89bfc1b77ada83f4fd5
Jonne Kaajalahti <jonne.kaajalahti@gmail.com>
2021-03-25 00:38:18 +0200

    Added basic search functionality for testing

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
 import "./App.css";
 
-import data from "./data/rikkeet.json";
+import rikeData from "./data/rikkeet.json";
 import sakkoData from "./data/sakot.json";
+import sakkoData2 from "./data/sakot2.json";
 import tuomioData from "./data/tuomiot.json";
 
 function App() {
-	console.log(data);
-	console.log(sakkoData);
-	console.log(tuomioData);
+	sakkoData2.forEach((element) => {
+		console.log(element);
+	});
 
-	const vanginKarkaaminen = data.rikoksetViranomaistaVastaan.find(
-		(e) => e.nimike === "Vangin karkaaminen"
-	);
+	const [keyword, setKeyword] = useState("");
+	let listData = [];
+
+	let filteredSakkodata = sakkoData2.filter((item) => {
+		return item.kategoria.toLowerCase().includes(keyword.toLowerCase());
+	});
+
+	keyword ? (listData = filteredSakkodata) : (listData = sakkoData2);
 
 	return (
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<Counter />
-				<p>
-					{vanginKarkaaminen.nimike} = {vanginKarkaaminen.sakko}€
-				</p>
+				{/* <Counter /> */}
+				<input
+					type="text"
+					id="header-search"
+					placeholder="Etsi..."
+					onChange={(e) => setKeyword(e.target.value)}
+					value={keyword}
+				/>
+
+				<ul>
+					{listData.map((item) => (
+						<li key={item.kategoria}>{item.kategoria}</li>
+					))}
+				</ul>
 			</header>
 		</div>
 	);
