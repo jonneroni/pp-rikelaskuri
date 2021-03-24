78e0497b190b43eba39082c22f6a170db7032ddb
Jonne Kaajalahti <jonne.kaajalahti@gmail.com>
2021-03-24 21:03:12 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+
+import data from "./data/rikkeet.json";
+import sakkoData from "./data/sakot.json";
+import tuomioData from "./data/tuomiot.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+	console.log(data);
+	console.log(sakkoData);
+	console.log(tuomioData);
+
+	const vanginKarkaaminen = data.rikoksetViranomaistaVastaan.find(
+		(e) => e.nimike === "Vangin karkaaminen"
+	);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<Counter />
+				<p>
+					{vanginKarkaaminen.nimike} = {vanginKarkaaminen.sakko}€
+				</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
