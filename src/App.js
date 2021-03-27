1c531bf215aeeba0af2c0077a076f88ca97999e8
Jonne Kaajalahti <jonne.kaajalahti@gmail.com>
2021-03-27 19:52:44 +0200

    Added initial collapsible components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
 import "./App.css";
+import Collapsible from "react-collapsible";
+
+import styled from "styled-components";
 
 import rikeData from "./data/rikkeet.json";
 import sakkoData from "./data/sakot.json";
@@ -12,6 +15,27 @@ function App() {
 	sakkoData2.forEach((element) => {
 		console.log(element);
 	});
+
+	const WrapperDiv = styled.div`
+		/* background-color: red; */
+		/* flex: 1; */
+		border: 5px solid blue;
+		margin: 30px 0;
+		height: 75%;
+		width: 90%;
+		position: fixed;
+		overflow: auto;
+		/**
+   * Overwrite the contentInner padding + border
+   * to ensure zero height.
+   */
+	`;
+
+	const StyledP = styled.p`
+		margin: 5% 0;
+		padding: 0;
+		border: 5px solid green;
+	`;
 
 	const [keyword, setKeyword] = useState("");
 	let listData = [];
@@ -25,7 +49,7 @@ function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
+				{/* <img src={logo} className="App-logo" alt="logo" /> */}
 				{/* <Counter /> */}
 				<input
 					type="text"
@@ -34,12 +58,27 @@ function App() {
 					onChange={(e) => setKeyword(e.target.value)}
 					value={keyword}
 				/>
-
-				<ul>
+				<WrapperDiv>
+					{listData.map((item) => (
+						<Collapsible
+							key={item.kategoria}
+							trigger={item.kategoria}
+							triggerStyle={{ backgroundColor: "cyan", textAlign: "left" }}
+							contentInnerClassName="CustomInnerContentCSS"
+							triggerClassName="CustomTriggerCSS"
+							overflowWhenOpen="auto"
+						>
+							{item.rikkeet.map((rike) => (
+								<StyledP>{`${rike.nimike} = ${rike.sakko}€`}</StyledP>
+							))}
+						</Collapsible>
+					))}
+				</WrapperDiv>
+				{/* <ul>
 					{listData.map((item) => (
 						<li key={item.kategoria}>{item.kategoria}</li>
 					))}
-				</ul>
+				</ul> */}
 			</header>
 		</div>
 	);
